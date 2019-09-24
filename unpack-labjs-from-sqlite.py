e305e74d6004d966cc62a6c814eb045d756afdda
Dan Fitch <dgfitch@gmail.com>
2019-09-24 09:10:31 -0500

    Correct-ish unpacking

--- unpack-labjs-from-sqlite.py
+++ unpack-labjs-from-sqlite.py
@@ -20,16 +20,11 @@ class Unpacker():
         return cur.fetchall()
 
     def unpack(self):
-        # Map of labjs session data
+        # Map of labjs session data by ppt id
         sessions = {}
-        # Map of labjs sessions to participant ids
-        session_to_ppt = {}
-        ppt = 0
-        for row in self.select("select * from labjs"):
+        for row in self.select("select * from labjs where metadata like '%\"payload\":\"full\"%'"):
             rowid = row[0]
             session = row[1]
-            if not session in sessions:
-                sessions[session] = {}
 
             # The data is a JSON-encoded array in the last column
             data = json.loads(row[5])
@@ -39,22 +34,24 @@ class Unpacker():
                     if 'ppt' in thing:
                         ppt = thing['ppt']
                         logging.info(f"PPT {ppt}: Reading row {rowid} for session {session}")
-                        session_to_ppt[session] = ppt
 
-                if thing['sender'] == 'Video - Continuous':
+                if thing['sender'] == 'Video':
                     count = 0
                     if 'trial_count' in thing:
                         count = thing['trial_count']
-                    logging.debug(f"Got video {count} with {len(thing['response'])} mouse movements")
-                    if not str(count) in sessions[session]:
+                    ppt = thing['ppt']
+                    if not ppt in sessions:
+                        sessions[ppt] = {}
+                    logging.debug(f"Got video {count} with {len(thing['response'])} mouse movements for {ppt}")
+                    if not str(count) in sessions[ppt]:
                         to_save = {
                                 'affect': thing['affect'],
-                                'response': thing['response']
+                                'response': thing['response'],
+                                'ppt': thing['ppt'],
                             }
                         if 'video_filename' in thing:
                             to_save['video_filename'] = thing['video_filename']
-                            logging.info(f'Got {thing["video_filename"]}')
-                        sessions[session][str(count)] = to_save
+                        sessions[ppt][str(count)] = to_save
 
         return sessions
         
