8fd01b91a9a73b7f0991fa0034a58b7563daf7e7
Dan Fitch <dgfitch@gmail.com>
2020-03-10 14:16:17 -0500

    Configurable sync script

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Analysis code for [uwmadison-chm/eat-labjs](https://github.com/uwmadison-chm/eat
 Currently computes Pearson correlations with original raters.
 
 Will probably do more stats stuff in the future.
+
+## Setup
+
+Edit sync.sh and change variables.
+
+Run sync.sh as studyname-upload in crontab on webtasks.
+
---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE="/var/www/www_root/qet/data/data.sqlite"
+DESTINATION="/study/EXAMPLE/raw_data/eat"
+STUDY="EXAMPLE"
+
+source /home/bashrc
+set_study $STUDY
+
+cp $SOURCE $DESTINATION
+chmod 700 $DESTINATION/data.sqlite
+cd "$(dirname "$0")"
+python3 unpack-labjs-from-sqlite.py $DESTINATION/data.sqlite $DESTINATION
